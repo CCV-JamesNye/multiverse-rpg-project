08e154687cf9ccf46d6b0249f644cab33daa1c34
Eric Rivera <ear00053@vsc.edu>
2026-03-22 19:08:59 -0400

    Image Movement and Slime Physics
    Moved the images for the Homeworld/Test World, and gave the Slime the ability to interact with the player.

---
+++ Scenes/Worlds/Homeworld/Enemies/Slime/Scripts/enemy_slime.gd
@@ -0,0 +1,8 @@
+extends CharacterBody2D
+
+@export var patrol_speed: float = 50.0
+var facing = "none"
+enum state {IDLE, PATROL, CHASE}
+var current_state : state = state.IDLE
+func _physics_process(delta: float) -> void:
+	move_and_slide()
