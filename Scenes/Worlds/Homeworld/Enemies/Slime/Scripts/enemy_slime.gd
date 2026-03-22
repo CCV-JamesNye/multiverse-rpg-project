194bfd648ba7b80dedba563262766787bce1a8ad
Eric Rivera <ear00053@vsc.edu>
2026-03-22 19:59:04 -0400

    Patrolling Slime
    Allowed the Slime to move while Patrolling.

--- Scenes/Worlds/Homeworld/Enemies/Slime/Scripts/enemy_slime.gd
+++ Scenes/Worlds/Homeworld/Enemies/Slime/Scripts/enemy_slime.gd
@@ -1,14 +1,26 @@
 extends CharacterBody2D
 
 @export var patrol_speed: float = 150.0
-var direction : Vector2 = Vector2.LEFT
+var direction : Vector2 = Vector2.DOWN
 var facing = "down"
 enum state {IDLE, PATROL, CHASE}
 var current_state : state = state.PATROL
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var wall_detector: RayCast2D = $WallDetector
 
 func _physics_process(delta: float) -> void:
 	move_and_collide(velocity * delta)
+	if wall_detector.is_colliding():
+		if direction == Vector2.DOWN:
+			direction = Vector2.UP
+			facing = "up"
+			wall_detector.position.y = -15
+			wall_detector.rotate(deg_to_rad(180))
+		elif direction == Vector2.UP:
+			direction = Vector2.DOWN
+			facing = "down"
+			wall_detector.position.y = 0
+			wall_detector.rotate(deg_to_rad(180))
 
 func _process(_delta: float) -> void:
 	if current_state == state.IDLE:
