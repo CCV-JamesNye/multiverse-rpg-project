c360c096b50bd3264d19c0a540857129450dc528
Eric Rivera <ear00053@vsc.edu>
2026-03-31 13:59:41 -0400

    Attempted Knockback
    Attempted to give the Player and Idle Chaser knockback. Will be reverted.

--- Scenes/Game Manager/Scripts/game_manager.gd
+++ Scenes/Game Manager/Scripts/game_manager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var enemy_direction
+
 func plr_die():
 	await SceneTransition.fade_to_black()
 	if get_tree() != null:
--- Scenes/Player/Scripts/player.gd
+++ Scenes/Player/Scripts/player.gd
@@ -7,14 +7,20 @@ var health : int = 10
 var max_health : int = 10
 var death_anim : bool = false
 signal health_update (int)
+var enemies
+var is_input_allowed : bool = true
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var hurtbox: Area2D = $HurtBox
+@onready var collision: CollisionShape2D = $Collision
+@onready var hit_timer: Timer = $HitTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	enemies = get_tree().get_first_node_in_group("enemies")
 	hurtbox.send_damage.connect(take_damage)
 	animated_sprite_2d.play("idle_down")
+	hit_timer.timeout.connect(stop_hit_movement)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,22 +29,22 @@ func _process(delta: float) -> void:
 	var direction : Vector2 = Vector2.ZERO
 	
 	# Reads input
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("move_right") && is_input_allowed == true:
 		facing = "right"
 		play_anim(1)
 		direction.x += 1
 		direction.y = 0
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("move_left") && is_input_allowed == true:
 		facing = "left"
 		play_anim(1)
 		direction.x -= 1
 		direction.y = 0
-	if Input.is_action_pressed("move_down"):
+	if Input.is_action_pressed("move_down") && is_input_allowed == true:
 		facing = "down"
 		play_anim(1)
 		direction.y += 1
 		direction.x = 0
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed("move_up") && is_input_allowed == true:
 		facing = "up"
 		play_anim(1)
 		direction.y -= 1
@@ -80,9 +86,17 @@ func play_anim(movement):
 
 func take_damage(damage: int) -> void:
 	health -= damage
+	if GameManager.enemy_direction == "down":
+		velocity.y += 150
+	is_input_allowed = false
+	hit_timer.start()
 	health_update.emit(health)
 	if health <= 0:
 		die()
 
 func die() -> void:
 	GameManager.plr_die()
+
+func stop_hit_movement() -> void:
+	velocity = Vector2.ZERO
+	is_input_allowed = true
