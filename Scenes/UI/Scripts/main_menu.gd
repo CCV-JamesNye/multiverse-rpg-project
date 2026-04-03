7d6b51d495575c2620d90de7ae011158b46a6459
Eric Rivera <ear00053@vsc.edu>
2026-04-02 20:25:04 -0400

    Button Sound
    Added a sound for pressing the buttons in the Main Menu.

--- Scenes/UI/Scripts/main_menu.gd
+++ Scenes/UI/Scripts/main_menu.gd
@@ -2,6 +2,8 @@ extends Control
 
 @onready var start_play: Button = $"MarginContainer/Panel/MarginContainer/VBoxContainer/Start-Play"
 @onready var quit_eject: Button = $"MarginContainer/Panel/MarginContainer/VBoxContainer/Quit-Eject"
+@onready var hover: AudioStreamPlayer2D = $Hover
+@onready var select: AudioStreamPlayer2D = $Select
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,9 +13,12 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func _quit_game() -> void:
+	select.play()
+	await SceneTransition.fade_to_black()
 	get_tree().quit()
 
 func _start_game() -> void:
+	select.play()
 	await SceneTransition.fade_to_black()
 	get_tree().change_scene_to_file("res://Scenes/Worlds/Test World/test_level.tscn")
 	pass
