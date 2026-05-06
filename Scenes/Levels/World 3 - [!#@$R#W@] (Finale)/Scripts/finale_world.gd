08c34dce52b736e5a870596813467fd80c9f0506
Eric Rivera <ear00053@vsc.edu>
2026-05-05 21:57:41 -0400

    Finale Progress and More BGM
    Finished three levels of [!#@$R#W@]. Also added more BGM.

---
+++ Scenes/Levels/World 3 - [!#@$R#W@] (Finale)/Scripts/finale_world.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	SceneTransition.save_scene()
+	SceneTransition.fade_in()
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
